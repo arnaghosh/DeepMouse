99041a36a67759a12022e6100e6b39efcef297c0
Arna Ghosh <arnatubaikgp@gmail.com>
2019-09-24 15:12:30 -0400

    adding createTrainValidationSplits function -- yet to be tested
    #Todo - add description of overlap_delta argument & test function on original dataset

--- CatCam/prepData.py
+++ CatCam/prepData.py
@@ -5,6 +5,7 @@ from os.path import isfile, join
 import imageio
 from scipy import ndimage
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
 def prepDataArrayFromMovie(movieName, num_cont_frames, overlap_delta = 0, temp_downsampling = 1, spatial_downsampling = 1):
@@ -12,6 +13,7 @@ def prepDataArrayFromMovie(movieName, num_cont_frames, overlap_delta = 0, temp_d
   '''
   movieName: Name of the folder to extract data frames from
   num_cont_frames: Number of contiguous frames to append to create a data frame
+  overlap_delta: 
   temp_downsampling: Temporal downsampling factor, picks every t_{th} frame from folder
   spatial_downsampling: Spatial downsampling factor, reduces image size by a factor of 's' (s < 1 for downsampling, s > 1 for upsampling)
   
@@ -68,12 +70,42 @@ def prepDataArrayFromMovie(movieName, num_cont_frames, overlap_delta = 0, temp_d
     
   return movie_seq
   
-def  createTrainValidationSplits(mode, split_ratio, save=False):
+def  createTrainValidationSplits(mode, split_ratio, num_cont_frames, overlap_delta = 0, temp_downsampling = 1, spatial_downsampling = 1, save=False):
   '''
   mode: 0 for keeping some data frames from each movie in validation set, 1 for keeping complete movies in validation set
   split_ratio: fraction of data to be kept in validation/testing set. Should be between 0 and 1
+  num_cont_frames: Number of contiguous frames to append to create a data frame
+  overlap_delta: 
+  temp_downsampling: Temporal downsampling factor, picks every t_{th} frame from folder
+  spatial_downsampling: Spatial downsampling factor, reduces image size by a factor of 's' (s < 1 for downsampling, s > 1 for upsampling)
   save: option to Save the training and validation splits to hdf5 file. Default value: False
   
   Output: two 4D numpy tensors/arrays containing positive samples, corresponding to training and validation splits respectively.
   '''
-  pass
+  movie_names_arr = ['movie1','movie2'] # populate this array correctly
+  if mode==1:
+    pass
+  else:   # keeping some data frames from each movie in traning and validation splits
+    all_movie_seqs = []
+    tr_movie_seqs = []
+    val_movie_seqs = []
+    for i in tqdm(len(movie_names_arr)):
+      movie_seq = prepDataArrayFromMovie(movie_names_arr[i],num_cont_frames,overlap_delta,temp_downsampling,spatial_downsampling)
+      all_movie_seqs.append(movie_seq)
+      shuffle_index = np.random.permutation(movie_seq.shape[0])
+      tr_index = shuffle_index[:int((1-split_ratio)*len(shuffle_index))]
+      val_index = shuffle_index[int((1-split_ratio)*len(shuffle_index)):]
+      tr_movie_seqs.append(movie_seq[tr_index])
+      val_movie_seqs.append(movie_seq[val_index])
+
+    all_movie_seqs = np.array(all_movie_seqs)
+    tr_movie_seqs = np.array(tr_movie_seqs)
+    val_movie_seqs = np.array(val_movie_seqs)
+
+  if save:
+    np.save('Training_set_seqs_'+str(num_cont_frames)+'_overlap_'+str(overlap_delta)+'_tempDown_'+str(temp_downsampling)+'_spatialDown_'+str(spatial_downsampling),tr_movie_seqs)
+    np.save('Validation_set_seqs_'+str(num_cont_frames)+'_overlap_'+str(overlap_delta)+'_tempDown_'+str(temp_downsampling)+'_spatialDown_'+str(spatial_downsampling),val_movie_seqs)
+    np.save('All_seqs_'+str(num_cont_frames)+'_overlap_'+str(overlap_delta)+'_tempDown_'+str(temp_downsampling)+'_spatialDown_'+str(spatial_downsampling),all_movie_seqs)
+
+  return [all_movie_seqs, tr_movie_seqs, val_movie_seqs]
+
